ffedea6b72725ad392d9e34dd2d1b72953c30a3c
williamsims <36799661+williamsims@users.noreply.github.com>
2018-06-11 12:31:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,15 +19,17 @@ var risk = ["low", "very high", "low", "medium", "very high", "medium", "low", "
               , "medium", "low", "low", "very low", "medium", "low", "low", "low", "low", "very low", "low", "low", "low", "low"
               , "medium", "low", "high", "low", "very low", "medium", "low", "low", "low", "low", "very high", "low", "very low"
               , "very low", "low", "low", "low", "medium", "medium", "very low", "low", "medium", "low", "very low", "high"];
-
+              
+// This variable (risk) is an array that stores the eartquake risk of every state in the states array
 var handlers = {
   'LaunchRequest': function () { this.emit(':tell', "No intent by that name"); 
-    
+//This will run when the user responds to Alexa and their response has no intent 
   },
   'introintent': function () {
       this.response.speak("Hi, what state would you like to know the risk of a Earthquake?").listen("Hi, what state would you like to know the risk of an Earthquake?");
       this.emit(':responseReady');},
-// This intent is what begins the conversation  when you call to the skill. When you call to the conversation this is what Alexa will ask.       
+// This intent is what begins the conversation when you open the skill. 
+// When you call to the conversation this is what Alexa will ask.       
     'list_states': function () {
      let list = "";
       for(let i = 0; i < stateList.length; i++) {
@@ -40,7 +42,8 @@ var handlers = {
         }
       this.response.speak("The states are " + list).listen("What state?");
       this.emit(':responseReady');},
-// This above intent is what a user can call for if they want a list of the states. The loop takes all the states in the array and will print them out together.       
+// This above intent is what a user can call for if they want a list of the states. 
+// The loop takes all the states in the array and will print them out together.       
     'choose_state' : function () {
       var temp = -1;
       let s1 = this.event.request.intent.slots.state.value;
@@ -60,7 +63,10 @@ var handlers = {
       this.response.speak("The risk of an Earthquake in " + s1 + " is " + s2 + ".");
       this.emit(':responseReady');
     },
-    
+// This above intent is used when the user responds to Alexa with the nam of a state. 
+// The for loop finds where the state is inside the states array and uses that location
+// to also get the risk value for the said state. Once the two pieces of information
+// are taken out of the arrays, Alexa will respond to the user with a sentence using those values.  
 
 };
 
